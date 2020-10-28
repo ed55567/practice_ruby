f67b95fae14fca841cbe4ca674618bf254204bbb
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-10-27 21:32:48 -0400

    still need changes

--- store_item.rb
+++ store_item.rb
@@ -1,17 +1,12 @@
 
-
-book1 = {"book_name" => "Harry Potter", "color" => "Green", "price" => 30}
-
-p book1["book_name"]
-p book1["color"]
-p book1["price"]
-  
-
-
-class book
+class book1
   def initalize(book_name,color,price)
     @book_name = input_book_name
     @color = input_color
     @price = input_price
   end
+end
   
+  book1 =book.new("HarryPotter", "Green", 30)
+
+  p book1
