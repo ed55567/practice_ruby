b55a16ded13a2c24cb8212a28a2531477325038d
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-10-28 20:46:07 -0400

    debug 3

--- store_item.rb
+++ store_item.rb
@@ -5,10 +5,20 @@
 #example_method("HarryPotter", "Green", 30)
 #=>
 
-puts "Hello!"
-greeting = gets.chomp()
-if greeting == "Arrr!"
-  puts "Go away, pirate."
+# puts "Hello!"
+# greeting = gets.chomp()
+# if greeting == "Arrr!"
+#   puts "Go away, pirate."
+# else
+#   puts "Greetings, hater of pirates!"
+# end
+
+puts "Greetings! What is your year of origin?"
+origin = gets.chomp
+if origin.to_i < 1900
+  puts "That's the past!"
+elsif origin.to_i > 1900 && origin.to_i < 2020
+  puts "That's the present!"
 else
-  puts "Greetings, hater of pirates!"
+  puts "That's the future!"
 end
